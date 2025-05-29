cdde3938cd4624368ea7e0be459a4647992c391a
Ye Feng <ye@evercoast.com>
2025-05-29 19:10:17 +1200

    Override new/delete operator to prevent being hijacked in Unreal iOS causing crash

--- src/corto_decoder_c.cpp
+++ src/corto_decoder_c.cpp
@@ -8,63 +8,97 @@
 * @Last Modified time: 2024-06-17 10:17:20
 */
 
+#include <memory>
+// PREVENT HIJACKING??? IT SOMEHOW DOES WORK IN IOS
+void* operator new(std::size_t size) {
+    return std::malloc(size);
+}
+
+void operator delete(void* ptr) noexcept {
+    std::free(ptr);
+}
+
 #include "corto_decoder_c.h"
 #include "decoder.h"
 
 using namespace crt;
 
+
 void *Corto_CreateDecoder(int length, const unsigned char* data, Corto_DecoderInfo* decoderInfo)
 {
-    Decoder* decoder = new Decoder(length, data);
     
-    decoderInfo->nvert = decoder->nvert;
-    decoderInfo->nface = decoder->nface;   
-    decoderInfo->hasNormal = decoder->data.count("normal") > 0 ? 1 : 0;
-    
-    return decoder;
+    try {
+        
+        Decoder* decoder = new Decoder(length, data);
+        
+        decoderInfo->nvert = decoder->nvert;
+        decoderInfo->nface = decoder->nface;
+        decoderInfo->hasNormal = decoder->hasAttr("normal") ? 1 : 0;
+        
+        return decoder;
+    }
+    catch (...) {
+        return nullptr;
+    }
 }
  
 void Corto_DestroyDecoder(void* decoder_handler)
 {
-    // Turns out C++ cannot properly delete a void pointer. You have to cast it to something C++-ish
-    Decoder* decoder = (Decoder*)decoder_handler;
-    delete decoder;
+    try
+    {
+        
+        
+        // Turns out C++ cannot properly delete a void pointer. You have to cast it to something C++-ish
+        Decoder* decoder = (Decoder*)decoder_handler;
+        delete decoder;
+    }
+    catch (...)
+    {
+        return;
+    }
 }
 
 int Corto_DecodeMesh(void* decoder_handler, Corto_Vector3* vertices, uint32_t* indices, Corto_Vector3* normals, Corto_Color* colors, Corto_Vector2* texcoord)
 {
-    Decoder* decoder = (Decoder*)decoder_handler;
-
-    if (!decoder || decoder->nface == 0)
+    try
     {
-        return -1;
+        Decoder* decoder = (Decoder*)decoder_handler;
+        
+        if (!decoder || decoder->nface == 0)
+        {
+            return -1;
+        }
+        else
+        {
+            decoder->setIndex(indices);
+        }
+        
+        if (decoder->nvert > 0)
+        {
+            decoder->setPositions((float*)vertices);
+        }
+        
+        if (decoder->hasAttr("normal"))
+        {
+            decoder->setNormals((float*)normals);
+        }
+        
+        if (decoder->hasAttr("color"))
+        {
+            decoder->setAttribute("color", (char*)colors, VertexAttribute::FLOAT);
+        }
+        
+        if (decoder->hasAttr("uv"))
+        {
+            decoder->setUvs((float*)texcoord);
+        }
+        
+        decoder->decode();
+        
+        return decoder->nface;
     }
-    else
+    catch(...)
     {
-        decoder->setIndex(indices);
+        return 0;
     }
-
-    if (decoder->nvert > 0)
-    {
-        decoder->setPositions((float*)vertices);
-    }
-
-    if (decoder->data.count("normal") > 0)
-    {
-        decoder->setNormals((float*)normals);
-    }
-
-    if (decoder->data.count("color") > 0)
-    {
-        decoder->setAttribute("color", (char*)colors, VertexAttribute::FLOAT);
-    }
-
-    if (decoder->data.count("uv") > 0)
-    {
-        decoder->setUvs((float*)texcoord);
-    }
-
-    decoder->decode();
-
-    return decoder->nface;
 }
